c4c121165d42a382583134921a8719911f45ad4f
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-11-02 14:38:25 +0100

    [files] Add prototypes for mandatory and advanced task functions

--- binary_trees.h
+++ binary_trees.h
@@ -33,7 +33,7 @@ typedef struct binary_tree_s heap_t;
 /* binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *);
 
-/* tasks */
+/* Mandatory tasks(files) prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -44,5 +44,17 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+/* Advanced tasks(files) prototypes*/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
 #endif
