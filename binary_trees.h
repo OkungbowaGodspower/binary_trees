37e6ce7494d1f95503d217abf64fe0cbc845695b
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-11-03 15:01:08 +0100

    Add functions prototypes for checking if a binary tree is complete, if it's a BST, if it's an AVL tree, and if it's a heap

--- binary_trees.h
+++ binary_trees.h
@@ -56,6 +56,33 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+int successor(bst_t *node);
+int two_children(bst_t *root);
+int remove_type(bst_t *root);
+int binary_tree_is_avl(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+void bal(avl_t **tree);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+int binary_tree_is_complete(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+void _preorder(heap_t *tree, heap_t **node, size_t height);
+void heapify(heap_t *root);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
 
 /* helper functions */
 int _pow_recursion(int x, int y);
@@ -63,5 +90,14 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
 size_t binary_tree_height(const binary_tree_t *tree);
+int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
+size_t binary_tree_size(const binary_tree_t *tree);
+int btib_helper(const binary_tree_t *tree, int min, int max);
+bst_t *bst_remove(bst_t *root, int value);
+int btia_helper(const binary_tree_t *tree, int min, int max);
+void sata_helper(avl_t **root, int *array, size_t lo, size_t hi);
+int btih_helper(const binary_tree_t *tree);
+int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
+size_t tree_size_h(const binary_tree_t *tree);
 
 #endif
